0f63321f01217cfffcb3c411f50c01aa4927badb
Gurgen Abramyan <gurgen.abramyan.1995@gmail.com>
2022-10-15 22:59:50 +0300

    Анализы добавлены в админ-панель.

--- app/adapters/storage/models/analyzes.py
+++ app/adapters/storage/models/analyzes.py
@@ -1,6 +1,14 @@
 import sqlalchemy as sa
+from sqlalchemy.orm import relationship
 
 from app.adapters.storage.models.base import BaseModel
+
+analyzes_types_analyzes_table = sa.Table(
+    "analyzes_types_analyzes",
+    BaseModel.metadata,
+    sa.Column("analysis_type_id", sa.ForeignKey("analyzes_types.id"), primary_key=True),
+    sa.Column("analysis_id", sa.ForeignKey("analyzes.id"), primary_key=True),
+)
 
 
 class AnalysisType(BaseModel):
@@ -11,6 +19,15 @@ class AnalysisType(BaseModel):
     id: int = sa.Column(sa.BigInteger(), primary_key=True, autoincrement=True)
     name: str = sa.Column(sa.String(length=30), nullable=False)
     description: str = sa.Column(sa.Text())
+
+    analyzes: "Analysis" = relationship(
+        "Analysis",
+        secondary=analyzes_types_analyzes_table,
+        back_populates="analyzes_types",
+    )
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Analysis(BaseModel):
@@ -24,10 +41,11 @@ class Analysis(BaseModel):
     period: str = sa.Column(sa.String(length=30), nullable=False)
     is_active: bool = sa.Column(sa.Boolean(), default=False)
 
+    analyzes_types: "AnalysisType" = relationship(
+        "AnalysisType",
+        secondary=analyzes_types_analyzes_table,
+        back_populates="analyzes",
+    )
 
-analyzes_types_analyzes_table = sa.Table(
-    "analyzes_types_analyzes",
-    BaseModel.metadata,
-    sa.Column("analysis_type_id", sa.ForeignKey("analyzes_types.id"), primary_key=True),
-    sa.Column("analysis_id", sa.ForeignKey("analyzes.id"), primary_key=True),
-)
+    def __str__(self) -> str:
+        return self.name
--- app/api/admin/routers/analyzes.py
+++ app/api/admin/routers/analyzes.py
@@ -1,5 +1,29 @@
 from app.adapters.storage.models.analyzes import Analysis, AnalysisType
 from sqladmin import ModelView
+from wtforms.fields import BooleanField
+
+
+class AnalysisTypeAdmin(ModelView, model=AnalysisType):
+    """Типы анализов в админ панели."""
+
+    name = "Тип анализа"
+    name_plural = "Типы анализов"
+    icon = "fa-solid fa-vials"
+
+    column_list = ("id", "name", "description")
+    column_labels = {
+        "id": "ID",
+        "name": "Название",
+        "description": "Описание",
+        "analyzes": "Анализы",
+    }
+
+    form_ajax_refs = {
+        "analyzes": {
+            "fields": ("id", "name", "preparation"),
+            "order_by": "name",
+        }
+    }
 
 
 class AnalysisAdmin(ModelView, model=Analysis):
@@ -7,7 +31,10 @@ class AnalysisAdmin(ModelView, model=Analysis):
 
     name = "Анализ"
     name_plural = "Анализы"
-    icon = "fa-solid fa-flask"
+    icon = "fa-solid fa-vial"
+
+    form_overrides = {"is_active": BooleanField}
+    form_widget_args = {"is_active": {"class": "form-check-input"}}
 
     column_list = ("id", "name", "preparation", "period", "is_active")
     column_labels = {
@@ -16,15 +43,12 @@ class AnalysisAdmin(ModelView, model=Analysis):
         "preparation": "Хз что это",
         "period": "Время проведения",
         "is_active": "Активно",
+        "analyzes_types": "Типы анализов",
     }
 
-
-class AnalysisTypeAdmin(ModelView, model=AnalysisType):
-    """Типы анализов в админ панели."""
-
-    name = "Тип анализа"
-    name_plural = "Типы анализов"
-    icon = "fa-solid fa-flask"
-
-    column_list = ("id", "name", "description")
-    column_labels = {"id": "ID", "name": "Название", "description": "Описание"}
+    form_ajax_refs = {
+        "analyzes_types": {
+            "fields": ("id", "name"),
+            "order_by": "name",
+        }
+    }
